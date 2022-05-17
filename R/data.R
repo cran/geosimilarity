0f14a43d834b819303769c2735f2856f22ca4080
Yongze Song <yongze.song@postgrad.curtin.edu.au>
2022-05-17 15:20:06 +0000

    version 1.1

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @title Spatial datasets of trace element Zn.
+#'
+#' @description Spatial datasets of trace element Zn.
+#'
+#' @name zn
+#' @format \code{zn}: A data frame of trace element Zn with 894 rows and 12 variables
+#' @docType data
+#' @author Yongze Song \email{yongze.song@curtin.edu.au}
+#' @keywords dataset trace elements
+"zn"
